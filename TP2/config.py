b67637ad6638e92fcc07f129214cffb5c062ce9b
jtmr05 <62111562+jtmr05@users.noreply.github.com>
2023-06-10 02:15:43 +0100

    added additional modules

---
+++ TP2/config.py
@@ -0,0 +1,9 @@
+'''the HuggingFace model to be used'''
+MODEL: str = 'small'
+
+HOSTNAME: str = '127.0.0.1'
+SERVER_PORT: int = 8888
+BUFFER_SIZE: int = 2048
+
+'''number of bytes used to represent an integer as a bytes object'''
+INT_SIZE: int = BUFFER_SIZE
---
+++ TP2/utils.py
@@ -0,0 +1,53 @@
+import io
+import typing
+import socket
+
+
+def as_bytes(s: str) -> bytes:
+    return s.encode('utf-8')
+
+
+def from_bytes(b: bytes) -> str:
+    return b.decode('utf-8')
+
+
+def annotate(src: typing.Any, *ansi_escape_codes: int) -> str:
+
+    length: int = len(ansi_escape_codes)
+    if length == 0:
+        return str(src)
+
+    str_buffer: io.StringIO = io.StringIO()
+
+    str_buffer.write(f"\033[{ansi_escape_codes[0]}")
+    for code in ansi_escape_codes[1:]:
+        str_buffer.write(f";{code}")
+    str_buffer.write(f"m{str(src)}\033[0m")
+
+    return str_buffer.getvalue()
+
+
+def read_reliably(s: socket.socket, size: int, yes: bool = False) -> bytes:
+    buffer: bytearray = bytearray(size)
+    view: memoryview = memoryview(buffer)
+    i: int = 0
+    while i < size:
+        b_read: int = s.recv_into(view[i:], size - i)
+        if b_read == 0:
+            break
+        i += b_read
+        if yes:
+            print(f"@read:: i: {i}, size: {size}")
+    return bytes(buffer)
+
+
+def write_reliably(s: socket.socket, buffer: typing.Union[bytearray, bytes], size: int) -> int:
+    view: memoryview = memoryview(buffer)
+    i: int = 0
+    while i < size:
+        b_sent: int = s.send(view[i:], size - i)
+        if b_sent == 0:
+            break
+        i += b_sent
+        #print(f"@write:: i: {i}, size: {size}")
+    return i
